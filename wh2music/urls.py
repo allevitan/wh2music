305c070792a40006a3967aa368c2a5b2bfe4a537
Abe Levitan <abe@Codarama.(none)>
2012-12-30 13:33:22 -0500

    Add django project

---
+++ wh2music/urls.py
@@ -0,0 +1,17 @@
+from django.conf.urls import patterns, include, url
+
+# Uncomment the next two lines to enable the admin:
+# from django.contrib import admin
+# admin.autodiscover()
+
+urlpatterns = patterns('',
+    # Examples:
+    # url(r'^$', 'wh2music.views.home', name='home'),
+    # url(r'^wh2music/', include('wh2music.foo.urls')),
+
+    # Uncomment the admin/doc line below to enable admin documentation:
+    # url(r'^admin/doc/', include('django.contrib.admindocs.urls')),
+
+    # Uncomment the next line to enable the admin:
+    # url(r'^admin/', include(admin.site.urls)),
+)
