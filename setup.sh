eadc2b63ecba653c930f060bfa4bd6cb412ea935
Abe Levitan <allevitan@gmail.com>
2013-06-02 23:12:56 -0400

    make setup script work on a mac

--- setup.sh
+++ setup.sh
@@ -1,5 +1,23 @@
 #!/bin/bash
-apt-get install mplayer python-virtualenv
+if  ! which mplayer > /dev/null || ! which virtualenv > /dev/null
+then
+    echo 'dependencies not satisfied... searching for a package manager'
+    if which apt-get
+    then
+	echo 'using apt-get to install mplayer, libevent, and virtualenv'
+	apt-get install mplayer python-virtualenv libevent-dev
+    elif which brew && which pip
+    then
+	echo 'using brew to install mplayer abd libevent and pip to install virtualenv'
+	brew install mplayer libevent
+	pip install virtualenv
+    else
+	echo 'No acceptible package manager found - install mplayer, virtualenv, and libevent by your own damn self'
+    fi
+else
+    echo 'Dependencies met - continue as planned'
+fi
+
 virtualenv venv
 source venv/bin/activate
 pip install -r requirements.txt
